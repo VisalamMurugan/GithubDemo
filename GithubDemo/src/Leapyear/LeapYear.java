d27a4dfa452367cdc699251c3244e65f1d348be3
VisalamMurugan <visalam.murugan@gmail.com>
2020-05-23 16:54:13 +0530

    V2.0
    Leapyear updated to 2020

--- GithubDemo/src/Leapyear/LeapYear.java
+++ GithubDemo/src/Leapyear/LeapYear.java
@@ -5,7 +5,7 @@ public class LeapYear {
 
 			    public static void main(String[] args) {
 
-		        int year = 2014;
+		        int year = 2020;
 		        boolean leap = false;
 
 		        if(year % 4 == 0)
