6b5bfad23ca7629b7cc8b759d893f885a22d496a
VisalamMurugan <visalam.murugan@gmail.com>
2020-06-21 11:15:17 +0530

    Update HelloWorld.java
    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Welocme to Github learning");
-		System.out.println("TQ);
+		System.out.println("Welocme to Github patch1");
+		
 	}
 }
